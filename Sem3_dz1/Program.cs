8776cb7d28725cd35fb0ca8960e949ed6fdea6b5
given <given@live.ru>
2022-09-23 01:26:54 +0600

    DZ 3 comleted

---
+++ Sem3_dz1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write ("Enter a five-digit number ");
+int x = Convert.ToInt32 (Console.ReadLine());
+
+if (x < 100000 & x > 9999)
+{
+    int a = x/10000%10;
+    int b = x/1000%10;
+    int c = x/10%10;
+    int d = x%10;
+    if (a == d & b == c) Console.Write ("Polindrome");
+    else Console.Write ("Not polindrome");
+}
+else Console.Write ("Error");
